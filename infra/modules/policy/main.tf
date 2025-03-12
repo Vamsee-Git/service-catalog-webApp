53997fe6e2af8e6786395c6167cce6ed7e182b2a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-12 22:08:27 +0530

    Update main.tf

--- infra/modules/policy/main.tf
+++ infra/modules/policy/main.tf
@@ -84,69 +84,7 @@ resource "aws_iam_policy" "ec2_servicecatalog_policy" {
 POLICY
 } 
 
-resource "aws_iam_policy" "ec2_servicecatalog_policy" {
-  name        = "hello-world"
-  description = "Policy to allow Service Catalog to create EC2 instances, access S3 artifacts, and manage resources"
-  policy      = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "ec2:*",
-            "Resource": "*"
-        },
-        {
-            "Sid": "AllowS3AccessForServiceCatalogProvisioning",
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": "*",
-            "Condition": {
-                "StringEquals": {
-                    "s3:ExistingObjectTag/servicecatalog:provisioning": "true"
-                }
-            }
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject",
-                "s3:ListBucket"
-            ],
-            "Resource": [
-                "arn:aws:s3:::*"
-            ]
-        },
-        {
-            "Sid": "AllowResourceGroupManagement",
-            "Effect": "Allow",
-            "Action": [
-                "resource-groups:CreateGroup",
-                "resource-groups:ListGroupResources",
-                "resource-groups:DeleteGroup",
-                "resource-groups:Tag",
-                "resource-groups:Untag"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Sid": "AllowTaggingPermissions",
-            "Effect": "Allow",
-            "Action": [
-                "tag:GetResources",
-                "tag:GetTagKeys",
-                "tag:GetTagValues",
-                "tag:TagResources",
-                "tag:UntagResources"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-POLICY
-} 
+
 resource "aws_iam_role" "launch_role" {
   name = "SCLaunch-HelloWorld"
   assume_role_policy = <<POLICY
