a38496cab3e2808b7317df8b5c05ffc4ab4efc33
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-12 21:48:41 +0530

    Update main.tf

--- infra/modules/policy/main.tf
+++ infra/modules/policy/main.tf
@@ -20,109 +20,213 @@ resource "aws_iam_user_policy_attachment" "example" {
   user       = var.user_name
   policy_arn = aws_iam_policy.service_catalog_user_policy.arn
 }
-#IAM Role
-resource "aws_iam_role" "service_catalog_role" {
-  name               = "SCLaunch-S3product-12"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "GivePermissionsToServiceCatalog",
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "servicecatalog.amazonaws.com"
+resource "aws_iam_policy" "ec2_servicecatalog_policy" {
+  name        = "hello-world"
+  description = "Policy to allow Service Catalog to create EC2 instances, access S3 artifacts, and manage resources"
+  policy      = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "ec2:*",
+            "Resource": "*"
         },
-        "Action" : "sts:AssumeRole"
-      },
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : "arn:aws:iam::664418994073:root"
+        {
+            "Sid": "AllowS3AccessForServiceCatalogProvisioning",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:ExistingObjectTag/servicecatalog:provisioning": "true"
+                }
+            }
         },
-        "Action" : "sts:AssumeRole",
-        "Condition" : {
-          "StringLike" : {
-            "aws:PrincipalArn" : [
-              "arn:aws:iam::664418994073:role/TerraformEngine/TerraformExecutionRole*",
-              "arn:aws:iam::664418994073:role/TerraformEngine/ServiceCatalogExternalParameterParserRole*",
-              "arn:aws:iam::664418994073:role/TerraformEngine/ServiceCatalogTerraformOSParameterParserRole*"
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
             ]
-          }
+        },
+        {
+            "Sid": "AllowResourceGroupManagement",
+            "Effect": "Allow",
+            "Action": [
+                "resource-groups:CreateGroup",
+                "resource-groups:ListGroupResources",
+                "resource-groups:DeleteGroup",
+                "resource-groups:Tag",
+                "resource-groups:Untag"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowTaggingPermissions",
+            "Effect": "Allow",
+            "Action": [
+                "tag:GetResources",
+                "tag:GetTagKeys",
+                "tag:GetTagValues",
+                "tag:TagResources",
+                "tag:UntagResources"
+            ],
+            "Resource": "*"
         }
-      }
     ]
-  })
 }
- 
- 
-# Attach the policy to the IAM role
-resource "aws_iam_role_policy_attachment" "attach_policy" {
-  policy_arn = aws_iam_policy.this.arn
-  role       = aws_iam_role.service_catalog_role.name
-}
- 
- 
-resource "aws_iam_policy" "this" {
-  name        = "S3ResourceCreationAndArtifactAccessPolicy12"
-  description = "create and manage S3 buckets"
- 
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "VisualEditor0",
-        "Effect" : "Allow",
-        "Action" : "s3:GetObject",
-        "Resource" : "*",
-        "Condition" : {
-          "StringEquals" : {
-            "s3:ExistingObjectTag/servicecatalog:provisioning" : "true"
-          }
+POLICY
+} 
+resource "aws_iam_role" "launch_role" {
+  name = "SCLaunch-HelloWorld"
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "GivePermissionsToServiceCatalog",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "servicecatalog.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::664418994073:root"
+            },
+            "Action": "sts:AssumeRole",
+            "Condition": {
+                "StringLike": {
+                    "aws:PrincipalArn": [
+                        "arn:aws:iam::664418994073:role/TerraformEngine/TerraformExecutionRole*",
+                        "arn:aws:iam::664418994073:role/TerraformEngine/ServiceCatalogExternalParameterParserRole*",
+                        "arn:aws:iam::664418994073:role/TerraformEngine/ServiceCatalogTerraformOSParameterParserRole*"
+                    ]
+                }
+            }
         }
-      },
-      {
-        "Action" : [
-          "s3:CreateBucket*",
-          "s3:DeleteBucket*",
-          "s3:Get*",
-          "s3:List*",
-          "s3:PutBucketTagging"
-        ],
-        "Resource" : "arn:aws:s3:::*",
-        "Effect" : "Allow"
-      },
-      {
-        "Action" : [
-          "resource-groups:CreateGroup",
-          "resource-groups:ListGroupResources",
-          "resource-groups:DeleteGroup",
-          "resource-groups:Tag"
-        ],
-        "Resource" : "*",
-        "Effect" : "Allow"
-      },
-      {
-        "Action" : [
-          "tag:GetResources",
-          "tag:GetTagKeys",
-          "tag:GetTagValues",
-          "tag:TagResources",
-          "tag:UntagResources"
-        ],
-        "Resource" : "*",
-        "Effect" : "Allow"
-      }
     ]
-  })
 }
- 
+POLICY
+} 
+resource "aws_iam_role_policy_attachment" "launch_role_policy_attachment" {
+  role       = aws_iam_role.launch_role.name
+  policy_arn = aws_iam_policy.ec2_servicecatalog_policy.arn
+}
+
+resource "aws_iam_policy" "ec2_servicecatalog_policy" {
+  name        = "hello-world"
+  description = "Policy to allow Service Catalog to create EC2 instances, access S3 artifacts, and manage resources"
+  policy      = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "ec2:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowS3AccessForServiceCatalogProvisioning",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:ExistingObjectTag/servicecatalog:provisioning": "true"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        },
+        {
+            "Sid": "AllowResourceGroupManagement",
+            "Effect": "Allow",
+            "Action": [
+                "resource-groups:CreateGroup",
+                "resource-groups:ListGroupResources",
+                "resource-groups:DeleteGroup",
+                "resource-groups:Tag",
+                "resource-groups:Untag"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AllowTaggingPermissions",
+            "Effect": "Allow",
+            "Action": [
+                "tag:GetResources",
+                "tag:GetTagKeys",
+                "tag:GetTagValues",
+                "tag:TagResources",
+                "tag:UntagResources"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+} 
+resource "aws_iam_role" "launch_role" {
+  name = "SCLaunch-HelloWorld"
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "GivePermissionsToServiceCatalog",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "servicecatalog.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::302263075199:root"
+            },
+            "Action": "sts:AssumeRole",
+            "Condition": {
+                "StringLike": {
+                    "aws:PrincipalArn": [
+                        "arn:aws:iam::664418994073:role/TerraformEngine/TerraformExecutionRole*",
+                        "arn:aws:iam::664418994073:role/TerraformEngine/ServiceCatalogExternalParameterParserRole*",
+                        "arn:aws:iam::664418994073:role/TerraformEngine/ServiceCatalogTerraformOSParameterParserRole*"
+                    ]
+                }
+            }
+        }
+    ]
+}
+POLICY
+} 
+resource "aws_iam_role_policy_attachment" "launch_role_policy_attachment" {
+  role       = aws_iam_role.launch_role.name
+  policy_arn = aws_iam_policy.ec2_servicecatalog_policy.arn
+}
+# Output the IAM Role ARN
+output "role_arn" {
+  value = aws_iam_role.launch_role.arn
+}
 output "policy_arn" {
   value = aws_iam_policy.this.arn
 }
- 
-# Output the IAM Role ARN
-output "role_arn" {
-  value = aws_iam_role.service_catalog_role.arn
-}
-
-
