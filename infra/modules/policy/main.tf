ac95858eb6e03e1e0b2e37aa009074cbe58ef950
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-13 07:40:37 +0530

    Update main.tf

--- infra/modules/policy/main.tf
+++ infra/modules/policy/main.tf
@@ -1,24 +1,6 @@
-resource "aws_iam_policy" "service_catalog_user_policy" {
-  name        = var.policy_name
-  description = var.policy_description
-  policy      = data.aws_iam_policy_document.service_catalog_user_policy.json
-}
-
-data "aws_iam_policy_document" "service_catalog_user_policy" {
-  statement {
-    actions = [
-      "servicecatalog:SearchProductsAsAdmin",
-      "servicecatalog:DescribeProduct",
-      "servicecatalog:ProvisionProduct",
-      "servicecatalog:DescribeProvisionedProduct"
-    ]
-    resources = ["*"]
-  }
-}
-
 resource "aws_iam_user_policy_attachment" "example" {
   user       = var.user_name
-  policy_arn = aws_iam_policy.service_catalog_user_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/var.policy_name"
 }
 resource "aws_iam_policy" "ec2_servicecatalog_policy" {
   name        = "hello-world"
